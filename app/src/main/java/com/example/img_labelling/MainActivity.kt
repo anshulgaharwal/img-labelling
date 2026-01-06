4548a5f6ad70a44c9504ddf4c744ff4660b3dbfc
anshulgaharwal <anshulgaharwal@gmail.com>
2026-01-07 00:10:13 +0530

    Initial commit

---
+++ app/src/main/java/com/example/img_labelling/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.img_labelling
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import java.io.IOException
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    // extension function to get bitmap from assets
+    fun Context.assetsToBitmap(fileName: String): Bitmap?{
+        return try {
+            with(assets.open(fileName)){
+                BitmapFactory.decodeStream(this)
+            }
+        } catch (e: IOException) { null }
+    }
+}
+
