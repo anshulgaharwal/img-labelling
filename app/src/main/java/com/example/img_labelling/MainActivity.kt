fd24aff2e7f992aa5de6889265a81db327940fe8
anshulgaharwal <anshulgaharwal@gmail.com>
2026-01-07 00:23:08 +0530

    added assets and modified mainactivty

--- app/src/main/java/com/example/img_labelling/MainActivity.kt
+++ app/src/main/java/com/example/img_labelling/MainActivity.kt
@@ -4,6 +4,9 @@ import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -14,6 +17,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val img: ImageView = findViewById(R.id.imageToLabel)
+        // assets folder image file name with extension
+        val fileName = "flower.jpg"
+        // get bitmap from assets folder
+        val bitmap: Bitmap? = assetsToBitmap(fileName)
+        bitmap?.apply {
+            img.setImageBitmap(this)
+        }
+        val txtOutput : TextView = findViewById(R.id.txtOutput)
+        val btn: Button = findViewById(R.id.btnTest)
     }
     // extension function to get bitmap from assets
     fun Context.assetsToBitmap(fileName: String): Bitmap?{
